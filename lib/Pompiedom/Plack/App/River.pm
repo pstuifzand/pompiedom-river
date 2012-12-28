4666170f3d1f08b9ad2797eaeeeea43add01778d
Peter Stuifzand <peter.stuifzand@gmail.com>
2012-12-28 16:45:11 +0100

    Remove original homepage
    This commit removes the homepage feed where all items from all feeds
    were shown. Use the individual rivers for each user instead.

--- lib/Pompiedom/Plack/App/River.pm
+++ lib/Pompiedom/Plack/App/River.pm
@@ -24,10 +24,7 @@ use parent 'Pompiedom::AppBase';
 use Plack::Util::Accessor 'config', 'river';
 use Plack::Session;
 use Plack::Request;
-use Encode;
-
-use Date::Period::Human;
-use Data::Dumper;
+use Plack::Response;
 
 sub init_handlers {
     my ($self) = @_;
@@ -36,12 +33,12 @@ sub init_handlers {
         my ($self, $env) = @_;
         my $session = Plack::Session->new($env);
         if ($session->get('logged_in')) {
-            my $params = $self->_build_messages_template_params($env);
-            return $self->render_template('pompiedom_river.tt', $params, $env);
+            my $res = Plack::Response->new;
+            my $username = $session->get('username');
+            $res->redirect('/user/'.$username.'/dashboard');
+            return $res->finalize;
         }
-        else {
-            return $self->render_template('index.tt', {}, $env);
-        }
+        return $self->render_template('index.tt', {}, $env);
     });
 
     $self->register_handler('GET', '/about', sub {
@@ -65,49 +62,6 @@ sub init_handlers {
     });
 
     return;
-}
-
-sub _build_messages_template_params {
-    my ($self, $env) = @_;
-
-    my $session = Plack::Session->new($env);
-    my $req = Plack::Request->new($env);
-
-    my $ft = DateTime::Format::RFC3339->new();
-    my $dp = Date::Period::Human->new({lang => 'en'});
-
-    my @messages;
-
-    for my $m ($self->river->messages) {
-        next if $env->{pompiedom_api}->{db}->HaveFeedItemSeen($m->{id});
-
-        # FIX for twitters feeds, which doesn't work
-        if ($m->{title} && $m->{message} && ($m->{title} eq $m->{message})) {
-            delete $m->{title};
-        }
-
-        $m->{datetime}       = $ft->parse_datetime($m->{timestamp});
-        $m->{human_readable} = ucfirst($dp->human_readable($m->{datetime}));
-
-        push @messages, $m;
-    }
-
-    #@messages = splice @messages, 0, 12;
-
-    my $url = $req->param('link') || $req->param('url');
-    $url = decode("UTF-8", $url);
-
-    return {
-        river    => $self->river,
-        messages => \@messages,
-        config   => $self->config,
-        args     => {
-            link  => $url,
-            title => decode("UTF-8", scalar $req->param('title')),
-            description  => decode("UTF-8", scalar $req->param('description')),
-        },
-        feeds => $env->{pompiedom_api}->UserFeeds($session->get('username')),
-    };
 }
 
 1;
